dfffb66c2b3d228af2cb27a536b7dc0562a40f82
mSpiker1 <mspiker@embarqmail.com>
2021-11-23 15:08:50 -0500

    added code for exercise26 and unit tests

--- exercise26/src/main/java/baseline/Solution26.java
+++ exercise26/src/main/java/baseline/Solution26.java
@@ -5,42 +5,60 @@
 
 package baseline;
 
+import java.util.Scanner;
+
 public class Solution26 {
     //main method, just grabs input/prompts for input and calls appropriate methods for calculation
     public static void main(String[] args){
         //initialize input scanner, create class object for paymentCalculator
+        Scanner input = new Scanner(System.in);
+        PaymentCalculator pc = new PaymentCalculator();
 
         //input/output sequence to get information from user
         //assign use inputs to double values as necessary
+        System.out.print("What is your balance? ");
+        double balance = Double.parseDouble(input.nextLine());
+        System.out.print("What is the APR on your card (as a percent)? ");
+        double dailyRate = (Double.parseDouble(input.nextLine()) * 0.01) / 365; //turn to percent while grabbing
+        System.out.print("What is the monthly payment you can make? ");
+        double monPay = Double.parseDouble(input.nextLine());
 
-        //call payOffTime with user-input parameters for dailyRate, balance, and monthlyPayment, assign to a string
+        //call calculateMonthsUntilPaidOff with user-input parameters, assign to a string
+        String numMonths = pc.calculateMonthsUntilPaidOff(dailyRate, balance, monPay);
 
         //output resulting string
+        System.out.printf("It will take you %s months to pay off this card.", numMonths);
     }
 }
 
 //class to handle monetary calculations
-class paymentCalculator{
+class PaymentCalculator {
     //method to calculate months to pay off a credit card given daily rate, current balance, and monthly payment amounts
-    public String payOffTime(double dailyRate, double balance, double monthlyPayment){
+    public String calculateMonthsUntilPaidOff(double dailyRate, double balance, double monPay){
         //calculate the amt of months left to make payments using the given formula
+        double numMonths = -1 * (Math.log(1 + (balance/monPay * (1 - Math.pow((1 + dailyRate), 30)))) /
+                Math.log(Math.pow((1 + dailyRate), 30)));
 
-        //round the resulting value up, per instructions
-
-        //use formatDollars method to properly format the value into a string
+        //use formatMonths method to properly format the value into a string
+        String finalMonths;
+        finalMonths = formatMonths(numMonths);
 
         //return the string
-        return null;
+        return finalMonths;
     }
 
     //method to properly format a double value to a whole number of months
-    public String formatDollars(double value){
+    public String formatMonths(double value){
+        //round the number up to the next whole number, regardless of decimal value
+        value = Math.ceil(value);
+
         //write the value to a string
+        String newValue = String.valueOf(value);
 
-        //change the string to a substring of itself before the decimal place
+        //split into an array of two strings, of the strings before and after the decimal
+        newValue = newValue.split("\\.")[0];
 
-
-        //return the string
-        return null;
+        //return the first string in the array
+        return newValue;
     }
 }
--- exercise26/src/test/java/baseline/Solution26Test.java
+++ exercise26/src/test/java/baseline/Solution26Test.java
@@ -4,5 +4,37 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class Solution26Test extends Solution26{
-    //incomplete, will write in for final commit
+    //test for calculateMonthsUntilPaidOff
+    @Test
+    void calculateMonthsUntilPaidOffTest(){
+        //PaymentCalculator class object
+        PaymentCalculator pc = new PaymentCalculator();
+
+        //create three double values of the example user input from the assignment
+        double dailyRate = (12 * 0.01) / 365;
+        double balance = 5000;
+        double monPay = 100;
+
+        //create a string that stores the result of the test method using the double parameters
+        String testMonths = pc.calculateMonthsUntilPaidOff(dailyRate, balance, monPay);
+
+        //assert that testMonths equals "70"
+        assertEquals("70", testMonths);
+    }
+
+    //test for formatMonths method
+    @Test
+    void formatMonthsTest(){
+        //PaymentCalculator class object
+        PaymentCalculator pc = new PaymentCalculator();
+
+        //create a double to test, equal to a large decimal that should be rounded up
+        double s = 9.00000000001;
+
+        //create a new string that stores the result of the test method using the previous string as its parameter
+        String testString = pc.formatMonths(s);
+
+        //test that testString is equal to "10"
+        assertEquals("10", testString);
+    }
 }
