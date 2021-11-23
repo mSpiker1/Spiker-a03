5ba9aeb180a7983759a81715c160259795cf4241
mSpiker1 <mspiker@embarqmail.com>
2021-11-23 16:36:43 -0500

    Added psuedocode for exercise 28

---
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,18 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Matthew Spiker
+ */
+
+package baseline;
+
+public class Solution28 {
+    public static void main(String[] args){
+        //initialize input scanner and sum variable
+
+        //for loop to loop 5 times [for(int i=0; i<5; i++)]
+            //prompt user for input
+            //add input (parsed to an integer) to the sum variable
+
+        //prints the sum variable
+    }
+}
