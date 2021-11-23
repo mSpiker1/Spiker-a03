f9ff510ff54f869e332cd047f09f799f11d334b0
mSpiker1 <mspiker@embarqmail.com>
2021-11-23 17:07:17 -0500

    added production code for exercise 28. No unit tests, the code literally just grabs input after prompting, adding each input on top of itself and outputs the total inputs.

--- exercise28/src/main/java/baseline/Solution28.java
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -5,14 +5,21 @@
 
 package baseline;
 
+import java.util.Scanner;
+
 public class Solution28 {
     public static void main(String[] args){
         //initialize input scanner and sum variable
+        Scanner input = new Scanner(System.in);
+        int sum = 0;
 
         //for loop to loop 5 times [for(int i=0; i<5; i++)]
-            //prompt user for input
-            //add input (parsed to an integer) to the sum variable
+        for(int i=0; i!=5; i++){
+            System.out.print("Enter a number: ");
+            sum += Integer.parseInt(input.nextLine());
+        }
 
         //prints the sum variable
+        System.out.printf("The total is %d", sum);
     }
 }
