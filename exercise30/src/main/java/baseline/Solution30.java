fb4beb314a95c2abab386646585e7a57a5222a19
mSpiker1 <mspiker@embarqmail.com>
2021-11-24 16:39:16 -0500

    Added production code for exercise 30

--- exercise30/src/main/java/baseline/Solution30.java
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -7,16 +7,25 @@ package baseline;
 public class Solution30 {
     public static void main(String[] args){
         //initializes spacer variable, used to determine the length of a number about to be output
+        String spacer;
 
         //top level for loop for the x in x*y, x starts at 1 and loops until x is less than or equal to 12
+        for(int i=1; i<=12; i++) {
             //lower level for loop for the y in x*y, y starts at 1 and loops until y is less than or equal to 12
+            for (int j = 1; j <= 12; j++) {
                 //print the product of x and y plus one blank space
+                System.out.print((i * j) + " ");
 
                 //set the value of spacer to the product of x and y as a string
+                spacer = String.valueOf(i * j);
 
                 //if the length of the spacer string is 1, print two blank spaces
-                //else if the length of the spacer string is 2, print one blank space
-
-            //move to the next line with a blank println statement
+                if (spacer.length() == 1) System.out.print("  ");
+                    //else if the length of the spacer string is 2, print one blank space
+                else if (spacer.length() == 2) System.out.print(" ");
+            }
+            //move to the next line with a blank println statement before looping again
+            System.out.println();
+        }
     }
 }
