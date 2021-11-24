ca8723a19cafcc4ff0049f4f77867d8ec28e232b
mSpiker1 <mspiker@embarqmail.com>
2021-11-24 18:12:30 -0500

    Added exercise35 production code and updated unit testing

--- exercise35/src/main/java/baseline/Solution35.java
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -11,34 +11,49 @@ import java.util.Random;
 
 public class Solution35 {
     //create a random object
+    private final Random rand = new Random();
 
     public static void main(String[] args){
         //Solution35 class object
+        Solution35 s = new Solution35();
 
         //initialize input scanner
+        Scanner input = new Scanner(System.in);
 
-        //create a new string "name" that is equal to " "
+        //create a new string "name"
+        String name = " ";
 
         //initialize a contestant arraylist
+        ArrayList<String> contestants = new ArrayList<>();
 
         //while loop to prompt for and grab input until the name string is blank
+        while(!name.equals("")){
             //prompt for user input
+            System.out.print("Enter a name: ");
 
             //get user input and store in name string
+            name = input.nextLine();
 
             //if name is not blank
+            if(!name.equals("")) {
                 //add the name to the contestant arraylist
+                contestants.add(name);
+            }
+        }
 
         //output the string returned from randString when contestants arraylist is given as a parameter
+        System.out.printf("The winner is... %s", s.randString(contestants));
     }
 
     //randomly picks a string from an array
     public String randString(List<String> stringList){
         //create a string "selected"
+        String selected;
 
         //set selected to a randomly picked string from the arraylist
+        selected = stringList.get(rand.nextInt(stringList.size()));
 
         //return the randomly selected string
-        return null;
+        return selected;
     }
 }
