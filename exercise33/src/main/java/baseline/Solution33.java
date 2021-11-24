68c023797b0fbdff574a60d96f233e5f1a998ab1
mSpiker1 <mspiker@embarqmail.com>
2021-11-24 17:01:27 -0500

    Added exercise 33 psuedocode

---
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,33 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Matthew Spiker
+ */
+
+package baseline;
+import java.util.Scanner;
+import java.util.Random;
+
+public class Solution33 {
+    //create a random object to use in methods in this class
+
+    //main method to prompt for, get, and respond to user input
+    public static void main(String[] args){
+        //initialize input scanner, random number generator, string array for eight ball answers
+
+        //prompt user for a question
+
+        //pass through user input
+
+        //print a string generated from eightBall method
+    }
+
+    //method to generate a random eight-ball response string
+    public static String eightBall(){
+        //create string array of eight-ball responses
+
+        //create an int from the random number generator
+
+        //return the string from the eight-ball array that corresponds to the random integer
+        return null;
+    }
+}
