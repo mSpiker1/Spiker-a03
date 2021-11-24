ccc1a3a2cdc07fd97a4d0b68e9685d9bb71621fd
mSpiker1 <mspiker@embarqmail.com>
2021-11-24 17:46:00 -0500

    Added production code for exercise 34

--- exercise34/src/main/java/baseline/Solution34.java
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -13,26 +13,39 @@ public class Solution34 {
     //main method
     public static void main(String[] args){
         //ArrayManager class object
+        ArrayManager am = new ArrayManager();
 
         //create an input scanner
+        Scanner input = new Scanner(System.in);
 
         //create a string array of employee names
+        String[] employeeArray = new String[] {"Steve Craft", "Zelda Hyrule", "Pit Icarus", "Alyx Vance", "Aloy Dawn"};
 
         //use arrayOutput method to output all current employee names
+        am.arrayOutput(employeeArray);
 
         //prompt user for an employee name to remove
+        System.out.print("\nEnter an employee name to remove: ");
 
         //save user input to a string "remove"
+        String remove = input.nextLine();
 
         //use arrayMatch method to get the number in the array that corresponds to user input, or size+1 if none match
+        int match = am.arrayMatch(employeeArray, remove);
 
         //if match is equal to size + 1
+        if(match == (employeeArray.length + 1)){
             //tell the user the name does not match, output all employee names again
+            System.out.println("That name does not match any names in the database.\n");
 
-        //else
+            //else
+        } else{
             //use removeArray method to remove a name from the employee array
+            employeeArray = am.removeArray(employeeArray, match);
+        }
 
         //output the full array
+        am.arrayOutput(employeeArray);
     }
 }
 
@@ -40,28 +53,41 @@ class ArrayManager{
     //method to remove a string from an array
     public String[] removeArray(String[] array, int remove){
         //convert array to a list of strings
+        List<String> list = new ArrayList<>(Arrays.asList(array));
 
         //remove string #remove from the list
+        list.remove(remove);
 
         //set the array equal to the edited list of strings
+        array = list.toArray(new String[0]);
 
         //return the array
-        return null;
+        return array;
     }
 
     //method using a for loop to display all strings in an array
     public void arrayOutput(String[] array){
         //output how many employees are in the list
+        System.out.printf("There are %d employees:%n", array.length);
 
         //use a for loop to output all employees in the array
+        for (String s : array) {
+            System.out.print(s + "\n");
+        }
     }
 
     //function to loop through a string array and try to match it with user a user input string
     public int arrayMatch(String[] array, String userInput){
         //use a for loop to go through each string in the array
+        for(int i=0; i<array.length; i++){
             //if the user input equals string i from the array
+            if(userInput.equals(array[i])){
                 //return i
+                return i;
+            }
+        }
 
         //if the for loop completes, return the length of the array + 1
+        return array.length+1;
     }
 }
