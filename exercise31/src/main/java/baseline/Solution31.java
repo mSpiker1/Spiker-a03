1ba22424d37b971f6bfc472031f4f93525887500
mSpiker1 <mspiker@embarqmail.com>
2021-11-24 16:50:56 -0500

    Added production code for exercise 31

--- exercise31/src/main/java/baseline/Solution31.java
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -10,45 +10,73 @@ import java.text.DecimalFormat;
 public class Solution31 {
     public static void main(String[] args){
         //initialize key variables, String age, String restingHR, double intensity
+        String age = "default";
+        String restingHR = "default";
+        double intensity = 0.55;
 
         //prompt user for age
+        System.out.print("Enter your age: ");
         //get age using getNumString method to ensure the input is numeric
+        age = getNumString(age);
 
         //prompt user for resting heart rate
+        System.out.print("Enter your resting heart rate: ");
         //get restingHR using getNumString method to ensure the input is numeric
+        restingHR = getNumString(restingHR);
 
         //print out top line of table using restingHR, age, and intensity strings
+        System.out.printf("%nResting Pulse: %s\tAge:%s%n%nIntensity\t| Rate%n------------|--------%n", restingHR, age);
 
         //output sequence in a for loop [for(int i=55; i<96; i+=5)]
+        for(int i = 55; i < 96; i += 5) {
             //output intensity level (i), and output bpm string returned from calling karvonen method with appropriate parameters
+            System.out.printf("%d%%\t\t\t| %s bpm%n", i, karvonen(age, restingHR, intensity));
+
             //raise intensity variable (double) by 0.05 before next loop
+            intensity += 0.05;
+        }
     }
 
     //function to test if a string is a number
     public static boolean isNum(String string){
-        //tries to parse the string into a double, returns false if it can't or if it can, but it is 0 or less
-        return false;
+        try{
+            //tries to parse the string into a double, returns false if it can't or if it can, but it is 0 or less
+            Integer.parseInt(string);
+            return Integer.parseInt(string) > 0;
+        } catch (NumberFormatException e){
+            return false;
+        }
     }
 
     //function that takes in age and pulse
     public static String karvonen(String age, String pulse, double intensity){
         //initialize a decimal formatter to use before returning the string with the result stored in it
+        DecimalFormat whole = new DecimalFormat("0");
 
         //does the math for Karvenon target heart rate using the given parameters on method call
+        double targetHR = (((220 - Integer.parseInt(age)) - Integer.parseInt(pulse)) * intensity) + Integer.parseInt(pulse);
 
         //return the number formatted properly as a string
-        return null;
+        return whole.format(targetHR);
     }
 
     public static String getNumString(String string){
         //initialize input scanner to take input
+        Scanner input = new Scanner(System.in);
 
         //while string is not a number
+        while(!isNum(string)) {
             //get user input and store in string variable
+            string = input.nextLine();
+
             //if string is still not a number
+            if(!isNum(string)) {
                 //output "Please only enter a whole number!\n"
+                System.out.println("Please only enter a whole number!");
+            }
+        }
 
         //returns the string once the while loop is exited
-        return null;
+        return string;
     }
 }
