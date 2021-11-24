36525b09668f922a03daee669724f31257feb1eb
mSpiker1 <mspiker@embarqmail.com>
2021-11-24 14:41:57 -0500

    Added production code for exercise 29

--- exercise29/src/main/java/baseline/Solution29.java
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -5,32 +5,53 @@
 
 package baseline;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Solution29 {
     //main input/output function
     public static void main(String[] args) {
         //initialize input scanner, decimal format, and rate variable
+        Scanner input = new Scanner(System.in);
+        DecimalFormat roundup = new DecimalFormat("0.#");
+        String rate = null;
 
         //while rate is not a valid number (determined by isNum method)
-            //print "What is the rate of return "
-            //if rate is not a valid number
-                //print "Sorry. That's not a valid input.\n"
+        while (!isNum(rate)) {
+            //prompt for user input
+            System.out.print("What is the rate of return? ");
+
+            //store user input in the rate string
+            rate = input.nextLine();
+
+            //if the rate string is still not a number, print an error message and loop back around to get new input
+            if (!isNum(rate)) System.out.print("Sorry. That's not a valid input.\n");
+        }
 
         //calls the years() function to calculate the amount of years
+        double years = years(rate);
 
         //format the final years String using the previously initialized decimal formatter
+        String finalYears = roundup.format(years);
 
         //print "It will take finalYears years to double your initial investment."
+        System.out.printf("It will take %s years to double your initial investment.", finalYears);
     }
 
     //function to calculate years
     public static double years(String rate){
         //return (72 / rate) as a double value
-        return 0;
+        return 72/Double.parseDouble(rate);
     }
 
     //function to test if a string is a number
     public static boolean isNum(String string){
-        //tries to parse the string into a double, returns false if it can't or if it can, but it is 0 or less
-        return false;
+        try{
+            //tries to parse the string into a double, returns false if it can't or if it can, but it is 0 or less
+            Double.parseDouble(string);
+            return Double.parseDouble(string) > 0;
+        } catch (NumberFormatException e){
+            return false;
+        }
     }
 }
