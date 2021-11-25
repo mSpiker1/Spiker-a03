5e19111c61d0df28423804d615df3d1319c6e7a1
mSpiker1 <mspiker@embarqmail.com>
2021-11-24 19:10:25 -0500

    Added production code for exercise 38

--- exercise38/src/main/java/baseline/Solution38.java
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -12,33 +12,52 @@ public class Solution38 {
     //main method
     public static void main(String[] args){
         //Solution38 class object
+        Solution38 s = new Solution38();
 
         //initialize input scanner
+        Scanner input = new Scanner(System.in);
 
         //input/output sequence with user
+        System.out.print("Enter a list of numbers, separated by spaces: ");
+        String nums = input.nextLine();
 
         //split the nums variable up into a string array separated by spaces
+        String[] numList = nums.split(" ");
 
         //create another array of type Integer to store even numbers as numList passes through filterEvenNumbers
+        Integer[] evenList = s.filterEvenNumbers(numList);
 
         //pretext for final output
+        System.out.print("The even numbers are: ");
 
         //for loop to output even numbers
+        for(int i=0; i<evenList.length; i++){
             //if not at the end of the even array, just output the number and a space
-        //else output the number and a period (end of the array, end of output)
+            if(i<evenList.length-1) System.out.print(evenList[i] + " ");
+                //else output the number and a period (end of the array, end of output)
+            else System.out.print(evenList[i] + ".");
+        }
     }
 
     //function to sort even numbers from a given string array
     public Integer[] filterEvenNumbers(String[] array){
         //create an integer array to use to store the even numbers in the given array
+        ArrayList<Integer> evenList = new ArrayList<>();
 
         //for each string in the array
+        for (String s : array) {
             //if the string, when parsed to an integer, is evenly divisible by 2
+            if (Integer.parseInt(s) % 2 == 0) {
                 //add the string parsed to an integer to the arraylist of even numbers
+                evenList.add(Integer.parseInt(s));
+            }
+        }
 
         //creates a new Integer array to be returned to the main function, built from the evenList arraylist
+        Integer[] evens = new Integer[evenList.size()];
+        evens = evenList.toArray(evens);
 
         //returns the even numbers in an integer array
-        return null;
+        return evens;
     }
 }
